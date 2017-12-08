ba5b3043fc3047843ff208f9513a6318229e78aa
blckwltz <32988951+blckwltz@users.noreply.github.com>
2017-12-08 11:32:36 +0300

    Изменения и дополнения
    Дописал скрипт, внес исправления в CSS, добавил style-min и js-min

--- js/script.js
+++ js/script.js
@@ -4,15 +4,26 @@ var datein = document.querySelector("[name=date-in]");
 var dateout = document.querySelector("[name=date-out]");
 var adults = document.querySelector("[name=adults]");
 var children = document.querySelector("[name=children]");
+var storageadults = localStorage.getItem("adults");
+var storagechildren = localStorage.getItem("children");
 
 find.addEventListener("click", function (evt) {
   evt.preventDefault();
-  form.classList.toggle("form-field-hide");
+  form.classList.toggle("form-field-show");
+  form.classList.remove("form-field-error");
+  datein.focus();
+  if (storageadults && storagechildren) {
+    adults.value = storageadults;
+    children.value = storagechildren;
+  }
 });
 
 form.addEventListener("submit", function (evt) {
   if (!datein.value || !dateout.value || !adults.value || !children.value) {
     evt.preventDefault();
+    form.classList.remove("form-field-error");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("form-field-error");
   } else {
     localStorage.setItem("adults", adults.value);
     localStorage.setItem("children", children.value);
